4429b1c31281e4d08d0779109cc0224291fc0a59
sosoyun7311 <83933776+sosoyun7311@users.noreply.github.com>
2021-07-13 12:38:42 +0900

    Add files via upload

---
+++ test/java/com/dm/nabidm/NabiDmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dm.nabidm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NabiDmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
